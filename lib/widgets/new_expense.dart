06af50eef64d30e6d1b6d577f5bbc0c507796947
Serenay <serenayygunes@gmail.com>
2024-03-07 22:17:09 +0300

    Amount TextField added

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -9,10 +9,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -28,11 +30,20 @@ class _NewExpenseState extends State<NewExpense> {
             //  keyboardType: TextInputType.text,
             decoration: const InputDecoration(label: Text('Title')),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            //  keyboardType: TextInputType.text,
+            decoration:
+                const InputDecoration(prefixText: '\$', label: Text('Amount')),
+          ),
           Row(
             children: [
+              TextButton(onPressed: () {}, child: const Text('Cancel')),
               ElevatedButton(
                   onPressed: () {
                     print(_titleController.text);
+                    print(_amountController.text);
                   },
                   child: const Text('Save expense'))
             ],
