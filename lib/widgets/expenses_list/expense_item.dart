aa58dec590526db8a87dcab10a3101880d26aff4
Serenay <serenayygunes@gmail.com>
2024-03-07 13:59:16 +0300

    Creating a custom list item

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,41 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Text(
+                    '\$${expense.amount.toStringAsFixed(2)}'), //12.3435 -- 12.34
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(expense.date.toString())
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
