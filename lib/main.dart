841de899dc0ae6443c18906319229a28ddc83b92
Serenay <serenayygunes@gmail.com>
2024-03-12 11:04:21 +0300

    Setting other elements theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,30 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme =
-    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 125, 216, 239));
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 125, 216, 239));
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-          colorScheme: kColorScheme,
-          appBarTheme: const AppBarTheme().copyWith(
-              backgroundColor: kColorScheme.onPrimaryContainer,
-              foregroundColor: kColorScheme.primaryContainer)),
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 14),
+            ),
+      ),
       home: const Expenses(),
     ),
   );
