55c9a623ad4abc0e1f0a9c3037456724d0e6b5fe
Serenay <serenayygunes@gmail.com>
2024-03-13 13:03:53 +0300

    Device Orientation used

--- lib/main.dart
+++ lib/main.dart
@@ -9,33 +9,34 @@ var kColorScheme =
 //     seedColor: const Color.fromARGB(255, 231, 239, 125),
 //     brightness: Brightness.dark);
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((fn) => runApp(
-            MaterialApp(
-              // darkTheme: ThemeData.dark().copyWith(),
-              theme: ThemeData().copyWith(
-                colorScheme: kColorScheme,
-                appBarTheme: const AppBarTheme().copyWith(
-                    backgroundColor: kColorScheme.onPrimaryContainer,
-                    foregroundColor: kColorScheme.primaryContainer),
-                cardTheme: const CardTheme().copyWith(
-                  color: kColorScheme.secondaryContainer,
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-                ),
-                elevatedButtonTheme: ElevatedButtonThemeData(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: kColorScheme.primaryContainer),
-                ),
-                textTheme: ThemeData().textTheme.copyWith(
-                      titleLarge: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          color: kColorScheme.onSecondaryContainer,
-                          fontSize: 16),
-                    ),
-              ),
-              home: const Expenses(),
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  //     .then((fn) =>
+  runApp(
+    MaterialApp(
+      // darkTheme: ThemeData.dark().copyWith(),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 16),
             ),
-          ));
+      ),
+      home: const Expenses(),
+    ),
+  );
+  //);
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -64,6 +64,9 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    print(MediaQuery.of(context).size.height);
+
     Widget mainContent = const Center(
       child: Text('No expenses found.Start adding some'),
     );
@@ -88,14 +91,25 @@ class _ExpensesState extends State<Expenses> {
               onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(
-            child: mainContent,
-          ),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(expenses: _registeredExpenses),
+                ),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            ),
     );
   }
 }
